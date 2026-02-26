db9472e69278bf0ccdeb06049fa19449f6e20ea0
cesar160 <ctoledovillarreal@gmail.com>
2026-02-25 20:21:12 -0600

    Proyecto inicializado

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+// src/theme/colors.ts
+export const Colors = {
+  primary: '#2E7D32', // El verde oscuro de tu Figma
+  primaryLight: '#4CAF50', // Un verde más claro
+  background: '#F5F5F5', // Gris claro de fondo
+  surface: '#FFFFFF',    // Tarjetas blancas
+  text: '#1A1A1A',       // Texto principal
+  textSecondary: '#666666', // Textos secundarios
+  border: '#E0E0E0',
+  danger: '#D32F2F',     // Rojo para alertas
+};
